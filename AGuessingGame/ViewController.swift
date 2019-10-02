29a5ab8bf44e8a455d856f1ccdc514892029bb1e
Trent Stanley <tstanley@lcs.on.ca>
2019-10-02 12:38:37 -0400

    .

--- AGuessingGame/ViewController.swift
+++ AGuessingGame/ViewController.swift
@@ -41,6 +41,16 @@ let targetNumber = Int.random(in: 1...100)
         
     }    
 
-
+    // Will be used to check a guess
+    @IBAction func checkGuess(_ sender: Any) {
+        
+        // Obtain the guess value from the text field
+        let guessText = submittedGuess.text!
+        let guessNumber = Int(guessText)!
+        
+        // For testing purposes, what was the guess?
+        print("For testing purposes, the guess made was \)
+    }
+    
 }
 
