6e2b654bb2ac956073e1f50635cfd19ce5c9ba29
Trent Stanley <tstanley@lcs.on.ca>
2019-10-03 21:01:59 -0400

    I am confused

--- AGuessingGame/ViewController.swift
+++ AGuessingGame/ViewController.swift
@@ -51,6 +51,15 @@ let targetNumber = Int.random(in: 1...100)
         // For testing purposes, what was the guess?
         print("For testing purposes, the guess made was \(guessNumber)")
         
+        // Give the appropriate feedback to the user
+        if guessNumber > targetNumber {
+            print("Guess lower next time")
+        } else if guessNumber < targetNumber
+        print("Guess higher next time")
+    } else {
+    print ("You are correct!")
+        }
+        
     }
     
 }
