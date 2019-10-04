b95028122c088b5c1108969bf43df71988fa92dd
Trent Stanley <tstanley@lcs.on.ca>
2019-10-04 14:46:29 -0400

    Made it speak "guess lower next time

--- AGuessingGame/ViewController.swift
+++ AGuessingGame/ViewController.swift
@@ -12,7 +12,7 @@ import AVFoundation
 class ViewController: UIViewController {
     
     // MARK: Properties
-let targetNumber = Int.random(in: 1...100)
+    let targetNumber = Int.random(in: 1...100)
     
     @IBOutlet weak var submittedGuess: UITextField!
     //MARK: Initializers
@@ -40,7 +40,7 @@ let targetNumber = Int.random(in: 1...100)
         synthesizer.speak(utterance)
         
     }    
-
+    
     // Will be used to check a guess
     @IBAction func checkGuess(_ sender: Any) {
         
@@ -54,13 +54,28 @@ let targetNumber = Int.random(in: 1...100)
         // Give the appropriate feedback to the user
         if guessNumber > targetNumber {
             print("Guess lower next time")
-        } else if guessNumber < targetNumber
-        print("Guess higher next time")
-    } else {
-    print ("You are correct!")
+            
+            // Make and object named "synthesizer", which is an instance of the
+            // class 'AVSpeechSynthesizer
+            let synthesizer = AVSpeechSynthesizer()
+            
+            // Make a string that contains what we want the computer to say
+            let message = "Guess lower next time"
+            
+            // Make an object named 'utterance', which is an instance of the class
+            // AVSpeechUtterance
+            let utterance = AVSpeechUtterance(string: message)
+            
+            // Speak the message
+            synthesizer.speak(utterance)
+        } else if guessNumber < targetNumber {
+            print("Guess higher next time")
+        } else {
+            
+            print ("You are correct!")
         }
         
     }
-    
+        
 }
 
